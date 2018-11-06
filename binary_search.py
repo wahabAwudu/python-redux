d17fb0b4dfcc27540e459eec2187facddd487fdf
wahabawudu <wahabawudu2020@gmail.com>
2018-11-06 11:50:16 +0000

    fixed binary search algorithm

--- binary_search.py
+++ binary_search.py
@@ -1,19 +1,44 @@
-# status: still debugging
-def binary_search(x, search_list):
+# status: still needs debugging
+def binary_search(item, search_list):
+    """
+    implements a binary search with an item
+    and an item list.
+    returns the position of the item if found in the list
+    """
     iterations = 1
-    left = 0
-    right = len(search_list)-1
-    mid = (right + left)//2
+    far_left = 0
+    far_right = len(search_list)-1
+    mid_position = (far_right + far_left)//2
 
-    while search_list[mid] != x:
-        if search_list[mid] <x:
-            left = mid + 1
-        else:
-            right = mid - 1
-        mid = (right + left)//2
+    while item != search_list[mid_position]:
+
+        if item < search_list[mid_position]:
+            far_left = 0
+            far_right = mid_position
+            for i in range(far_left, far_right):
+                if item == search_list[i]:
+                    return i
+                    break
+                i = i+1
+        elif item > search_list[mid_position]:
+            far_left = + 1
+            far_right = len(search_list)
+
+            for i in range(far_left, far_right):
+                if item == search_list[i]:
+                    return i
+                    break
+                i + i+1
+
         iterations += 1
-    print('iterations = ', str(iterations))
-    return mid
 
-# will run into an error. still debugging.
-print(binary_search(32, [4,8,9,10,24,45,50,56,32]))
+    print('number of searches = ', str(iterations))
+    return mid_position
+
+
+item = 10
+search_list = [4,8,9,10,24,45,30,50,32,56]
+sorted_search_list = sorted(search_list)
+
+print(sorted_search_list)
+print(binary_search(item, sorted_search_list))
